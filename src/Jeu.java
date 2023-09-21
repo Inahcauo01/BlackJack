e105559d4a7786a4a19010faf927f8e9b0c11221
Inahcauo01 <tarek.ouachani01@gmail.com>
2023-09-21 11:26:53 +0100

    creer la methode extraire ieme carte (recuperer_carte)

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Jeu {
     //private List<Carte> cartes;     //objet carte
@@ -28,15 +29,29 @@ public class Jeu {
         for (int c=couleur; c<=4; c++){
             for (int h=hauteur; h<=13; h++){
                 //cartes.add(new Carte(h, c));  //objet carte
+                List<Integer> carte = new ArrayList<>();
+                carte.add(h);
+                carte.add(c);
+                cartes.add(carte);
             }
             hauteur = 1;
         }
     }
 
-
     //public List<Carte> getCartes() {  //objet carte
-        public List<List<Integer>> getCartes() {
-
+    public List<List<Integer>> getCartes() {
         return cartes;
+    }
+
+    public List<Object> recuperer_carte(int indice, List<List<Integer>> liste){
+        List<Object> resultatListe = new ArrayList<>();
+
+        if (indice >0 && indice <= liste.size()){
+            resultatListe.add(liste.get(indice-1));
+            liste.remove(indice-1);
+            resultatListe.add(liste);
+        }
+
+        return resultatListe;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        Jeu jeu = new Jeu();
+        Jeu jeu = new Jeu(10,4);
 
         List<List<Integer>> cartes = jeu.getCartes();
 
         cartes.stream().collect(Collectors.toList()).forEach(System.out::println);
+
+        List<Object> listeresult = jeu.recuperer_carte(2,cartes);
+        System.out.println("----------------\n Apres la recuperation : (indice 2)");
+        listeresult.stream().collect(Collectors.toList()).forEach(System.out::println);
     }
 
 }
